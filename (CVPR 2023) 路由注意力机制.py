297e23a77599256eb5665d46e44b4996cef1e45c
AIFengheshu <dingqs.ctgu@vip.163.com>
2024-11-04 13:15:01 +0800

    Update (CVPR 2023) 路由注意力机制.py

--- (CVPR 2023) 路由注意力机制.py
+++ (CVPR 2023) 路由注意力机制.py
@@ -1,58 +1,52 @@
-from typing import Tuple, Optional
-
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from einops import rearrange
 from torch import Tensor, LongTensor
+from typing import Tuple, Optional
 
-
-# Github地址：https://github.com/rayleizhu/BiFormer
-# 论文地址：https://arxiv.org/pdf/2303.08810
+# 论文题目：BiFormer: Vision Transformer with Bi-Level Routing Attention
+# 中文题目:  BiFormer:具有双层路由注意力的视觉Transformer
+# 论文链接：https://arxiv.org/pdf/2303.08810
+# 官方github：https://github.com/rayleizhu/BiFormer
 # 微信公众号：AI缝合术
-"""
-2024年全网最全即插即用模块,全部免费!包含各种卷积变种、最新注意力机制、特征融合模块、上下采样模块，
-适用于人工智能(AI)、深度学习、计算机视觉(CV)领域，适用于图像分类、目标检测、实例分割、语义分割、
-单目标跟踪(SOT)、多目标跟踪(MOT)、红外与可见光图像融合跟踪(RGBT)、图像去噪、去雨、去雾、去模糊、超分等任务，
-模块库持续更新中......
-https://github.com/AIFengheshu/Plug-play-modules
-"""
+
 class TopkRouting(nn.Module):
     """
-    differentiable topk routing with scaling
-    Args:
-        qk_dim: int, feature dimension of query and key
-        topk: int, the 'topk'
-        qk_scale: int or None, temperature (multiply) of softmax activation
-        with_param: bool, wether inorporate learnable params in routing unit
-        diff_routing: bool, wether make routing differentiable
-        soft_routing: bool, wether make output value multiplied by routing weights
+    可微分的 Topk 路由与缩放
+    参数:
+        qk_dim: int, 查询和键的特征维度
+        topk: int, 表示 'topk' 值
+        qk_scale: int 或 None，softmax 激活的温度（乘数）
+        with_param: bool，是否在路由单元中加入可学习参数
+        diff_routing: bool，是否让路由变为可微分
+        soft_routing: bool，是否让输出值与路由权重相乘
     """
 
     def __init__(self, qk_dim, topk=4, qk_scale=None, param_routing=False, diff_routing=False):
         super().__init__()
         self.topk = topk
         self.qk_dim = qk_dim
-        self.scale = qk_scale or qk_dim ** -0.5
+        self.scale = qk_scale or qk_dim ** -0.5  # 计算缩放系数
         self.diff_routing = diff_routing
-        # TODO: norm layer before/after linear?
-        self.emb = nn.Linear(qk_dim, qk_dim) if param_routing else nn.Identity()
-        # routing activation
+        # TODO: 在线性层之前/之后添加规范层？
+        self.emb = nn.Linear(qk_dim, qk_dim) if param_routing else nn.Identity()  # 可学习参数或恒等变换
+        # 路由激活函数
         self.routing_act = nn.Softmax(dim=-1)
 
     def forward(self, query: Tensor, key: Tensor) -> Tuple[Tensor]:
         """
-        Args:
-            q, k: (n, p^2, c) tensor
-        Return:
-            r_weight, topk_index: (n, p^2, topk) tensor
+        参数:
+            query, key: (n, p^2, c) 张量
+        返回:
+            r_weight, topk_index: (n, p^2, topk) 张量
         """
         if not self.diff_routing:
-            query, key = query.detach(), key.detach()
-        query_hat, key_hat = self.emb(query), self.emb(key)  # per-window pooling -> (n, p^2, c)
-        attn_logit = (query_hat * self.scale) @ key_hat.transpose(-2, -1)  # (n, p^2, p^2)
-        topk_attn_logit, topk_index = torch.topk(attn_logit, k=self.topk, dim=-1)  # (n, p^2, k), (n, p^2, k)
-        r_weight = self.routing_act(topk_attn_logit)  # (n, p^2, k)
+            query, key = query.detach(), key.detach()  # 若不可微分路由，则分离梯度
+        query_hat, key_hat = self.emb(query), self.emb(key)  # 对每个窗口进行池化 -> (n, p^2, c)
+        attn_logit = (query_hat * self.scale) @ key_hat.transpose(-2, -1)  # 计算注意力 logits -> (n, p^2, p^2)
+        topk_attn_logit, topk_index = torch.topk(attn_logit, k=self.topk, dim=-1)  # 取 topk 值及其索引
+        r_weight = self.routing_act(topk_attn_logit)  # softmax 激活 -> (n, p^2, k)
 
         return r_weight, topk_index
 
@@ -61,104 +55,94 @@ class KVGather(nn.Module):
     def __init__(self, mul_weight='none'):
         super().__init__()
         assert mul_weight in ['none', 'soft', 'hard']
-        self.mul_weight = mul_weight
+        self.mul_weight = mul_weight  # 路由权重类型
 
     def forward(self, r_idx: Tensor, r_weight: Tensor, kv: Tensor):
         """
-        r_idx: (n, p^2, topk) tensor
-        r_weight: (n, p^2, topk) tensor
-        kv: (n, p^2, w^2, c_kq+c_v)
+        r_idx: (n, p^2, topk) 张量
+        r_weight: (n, p^2, topk) 张量
+        kv: (n, p^2, w^2, c_kq+c_v) 张量
 
-        Return:
-            (n, p^2, topk, w^2, c_kq+c_v) tensor
+        返回:
+            (n, p^2, topk, w^2, c_kq+c_v) 张量
         """
-        # select kv according to routing index
+        # 根据路由索引选择 kv
         n, p2, w2, c_kv = kv.size()
         topk = r_idx.size(-1)
-        # print(r_idx.size(), r_weight.size())
-        # FIXME: gather consumes much memory (topk times redundancy), write cuda kernel?
+        # FIXME: gather 占用较多内存（topk 倍冗余），考虑编写 cuda 内核优化？
         topk_kv = torch.gather(kv.view(n, 1, p2, w2, c_kv).expand(-1, p2, -1, -1, -1),
-                               # (n, p^2, p^2, w^2, c_kv) without mem cpy
+                               # (n, p^2, p^2, w^2, c_kv) 无需内存复制
                                dim=2,
                                index=r_idx.view(n, p2, topk, 1, 1).expand(-1, -1, -1, w2, c_kv)
                                # (n, p^2, k, w^2, c_kv)
                                )
 
         if self.mul_weight == 'soft':
-            topk_kv = r_weight.view(n, p2, topk, 1, 1) * topk_kv  # (n, p^2, k, w^2, c_kv)
+            topk_kv = r_weight.view(n, p2, topk, 1, 1) * topk_kv  # 使用 soft 权重与 kv 相乘
         elif self.mul_weight == 'hard':
-            raise NotImplementedError('differentiable hard routing TBA')
-        # else: #'none'
-        #     topk_kv = topk_kv # do nothing
-
+            raise NotImplementedError('可微分的硬路由尚未实现')
+        # 否则: 'none' 跳过权重操作
+        #     topk_kv = topk_kv
         return topk_kv
-
 
 class QKVLinear(nn.Module):
     def __init__(self, dim, qk_dim, bias=True):
         super().__init__()
+        # 初始化 QKV 的线性变换层
         self.dim = dim
         self.qk_dim = qk_dim
+        # 定义线性层，将输入维度映射到 q, k, v 维度
         self.qkv = nn.Linear(dim, qk_dim + qk_dim + dim, bias=bias)
 
     def forward(self, x):
+        # 分割线性层输出为 q 和 kv
         q, kv = self.qkv(x).split([self.qk_dim, self.qk_dim + self.dim], dim=-1)
         return q, kv
-        # q, k, v = self.qkv(x).split([self.qk_dim, self.qk_dim, self.dim], dim=-1)
-        # return q, k, v
-
 
 class BiLevelRoutingAttention(nn.Module):
     """
-    n_win: number of windows in one side (so the actual number of windows is n_win*n_win)
-    kv_per_win: for kv_downsample_mode='ada_xxxpool' only, number of key/values per window. Similar to n_win, the actual number is kv_per_win*kv_per_win.
-    topk: topk for window filtering
-    param_attention: 'qkvo'-linear for q,k,v and o, 'none': param free attention
-    param_routing: extra linear for routing
-    diff_routing: wether to set routing differentiable
-    soft_routing: wether to multiply soft routing weights
+    双层路由注意力机制模块，用于局部和全局注意力的计算
     """
-
     def __init__(self, dim, n_win=7, num_heads=8, qk_dim=None, qk_scale=None,
                  kv_per_win=4, kv_downsample_ratio=4, kv_downsample_kernel=None, kv_downsample_mode='identity',
                  topk=4, param_attention="qkvo", param_routing=False, diff_routing=False, soft_routing=False,
                  side_dwconv=3,
                  auto_pad=True):
         super().__init__()
-        # local attention setting
+        # 初始化局部注意力的基本配置
         self.dim = dim
-        self.n_win = n_win  # Wh, Ww
+        self.n_win = n_win  # 窗口大小
         self.num_heads = num_heads
         self.qk_dim = qk_dim or dim
-        assert self.qk_dim % num_heads == 0 and self.dim % num_heads == 0, 'qk_dim and dim must be divisible by num_heads!'
-        self.scale = qk_scale or self.qk_dim ** -0.5
+        # 确保 qk_dim 和 dim 可以整除 num_heads
+        assert self.qk_dim % num_heads == 0 and self.dim % num_heads == 0, 'qk_dim 和 dim 必须能被 num_heads 整除'
+        self.scale = qk_scale or self.qk_dim ** -0.5  # qk_scale 缩放因子
 
-        ################side_dwconv (i.e. LCE in ShuntedTransformer)###########
+        # 侧边深度卷积，用于捕捉位置信息
         self.lepe = nn.Conv2d(dim, dim, kernel_size=side_dwconv, stride=1, padding=side_dwconv // 2,
                               groups=dim) if side_dwconv > 0 else \
             lambda x: torch.zeros_like(x)
 
-        ################ global routing setting #################
+        # 路由配置
         self.topk = topk
         self.param_routing = param_routing
         self.diff_routing = diff_routing
         self.soft_routing = soft_routing
-        # router
-        assert not (self.param_routing and not self.diff_routing)  # cannot be with_param=True and diff_routing=False
-        self.router = TopkRouting(qk_dim=self.qk_dim,
-                                  qk_scale=self.scale,
-                                  topk=self.topk,
-                                  diff_routing=self.diff_routing,
-                                  param_routing=self.param_routing)
-        if self.soft_routing:  # soft routing, always diffrentiable (if no detach)
+        # 初始化路由器
+        assert not (self.param_routing and not self.diff_routing), '不能设置为参数化但非差分路由'
+        self.router = TopkRouting(qk_dim=self.qk_dim, qk_scale=self.scale, topk=self.topk,
+                                  diff_routing=self.diff_routing, param_routing=self.param_routing)
+
+        # 配置路由权重模式
+        if self.soft_routing:
             mul_weight = 'soft'
-        elif self.diff_routing:  # hard differentiable routing
+        elif self.diff_routing:
             mul_weight = 'hard'
-        else:  # hard non-differentiable routing
+        else:
             mul_weight = 'none'
         self.kv_gather = KVGather(mul_weight=mul_weight)
 
-        # qkv mapping (shared by both global routing and local attention)
+        # 定义 qkv 映射层
         self.param_attention = param_attention
         if self.param_attention == 'qkvo':
             self.qkv = QKVLinear(self.dim, self.qk_dim)
@@ -167,125 +151,84 @@ class BiLevelRoutingAttention(nn.Module):
             self.qkv = QKVLinear(self.dim, self.qk_dim)
             self.wo = nn.Identity()
         else:
-            raise ValueError(f'param_attention mode {self.param_attention} is not surpported!')
+            raise ValueError(f'不支持的 param_attention 模式 {self.param_attention}')
 
+        # 设置 kv 下采样模式
         self.kv_downsample_mode = kv_downsample_mode
-        self.kv_per_win = kv_per_win
-        self.kv_downsample_ratio = kv_downsample_ratio
-        self.kv_downsample_kenel = kv_downsample_kernel
         if self.kv_downsample_mode == 'ada_avgpool':
-            assert self.kv_per_win is not None
             self.kv_down = nn.AdaptiveAvgPool2d(self.kv_per_win)
         elif self.kv_downsample_mode == 'ada_maxpool':
-            assert self.kv_per_win is not None
             self.kv_down = nn.AdaptiveMaxPool2d(self.kv_per_win)
         elif self.kv_downsample_mode == 'maxpool':
-            assert self.kv_downsample_ratio is not None
             self.kv_down = nn.MaxPool2d(self.kv_downsample_ratio) if self.kv_downsample_ratio > 1 else nn.Identity()
         elif self.kv_downsample_mode == 'avgpool':
-            assert self.kv_downsample_ratio is not None
             self.kv_down = nn.AvgPool2d(self.kv_downsample_ratio) if self.kv_downsample_ratio > 1 else nn.Identity()
-        elif self.kv_downsample_mode == 'identity':  # no kv downsampling
+        elif self.kv_downsample_mode == 'identity':
             self.kv_down = nn.Identity()
-        elif self.kv_downsample_mode == 'fracpool':
-            # assert self.kv_downsample_ratio is not None
-            # assert self.kv_downsample_kenel is not None
-            # TODO: fracpool
-            # 1. kernel size should be input size dependent
-            # 2. there is a random factor, need to avoid independent sampling for k and v
-            raise NotImplementedError('fracpool policy is not implemented yet!')
-        elif kv_downsample_mode == 'conv':
-            # TODO: need to consider the case where k != v so that need two downsample modules
-            raise NotImplementedError('conv policy is not implemented yet!')
         else:
-            raise ValueError(f'kv_down_sample_mode {self.kv_downsaple_mode} is not surpported!')
+            raise ValueError(f'不支持的 kv_downsample_mode 模式 {self.kv_downsample_mode}')
 
-        # softmax for local attention
+        # 定义 softmax 激活函数用于局部注意力
         self.attn_act = nn.Softmax(dim=-1)
-
-        self.auto_pad = auto_pad
+        self.auto_pad = auto_pad  # 自动填充标志
 
     def forward(self, x, ret_attn_mask=False):
         """
-        x: NHWC tensor
-
-        Return:
-            NHWC tensor
+        前向传播函数，输入和输出的格式为 NHWC
         """
+        # 将输入重排为 NHWC 格式
         x = rearrange(x, "n c h w -> n h w c")
-        # NOTE: use padding for semantic segmentation
-        ###################################################
+
         if self.auto_pad:
             N, H_in, W_in, C = x.size()
-
             pad_l = pad_t = 0
             pad_r = (self.n_win - W_in % self.n_win) % self.n_win
             pad_b = (self.n_win - H_in % self.n_win) % self.n_win
-            x = F.pad(x, (0, 0,  # dim=-1
-                          pad_l, pad_r,  # dim=-2
-                          pad_t, pad_b))  # dim=-3
-            _, H, W, _ = x.size()  # padded size
+            x = F.pad(x, (0, 0, pad_l, pad_r, pad_t, pad_b))
+            _, H, W, _ = x.size()
         else:
             N, H, W, C = x.size()
-            assert H % self.n_win == 0 and W % self.n_win == 0  #
-        ###################################################
+            assert H % self.n_win == 0 and W % self.n_win == 0
 
-        # patchify, (n, p^2, w, w, c), keep 2d window as we need 2d pooling to reduce kv size
+        # 将特征切割为窗口，方便后续局部操作
         x = rearrange(x, "n (j h) (i w) c -> n (j i) h w c", j=self.n_win, i=self.n_win)
 
-        #################qkv projection###################
-        # q: (n, p^2, w, w, c_qk)
-        # kv: (n, p^2, w, w, c_qk+c_v)
-        # NOTE: separte kv if there were memory leak issue caused by gather
+        # 计算 q 和 kv
         q, kv = self.qkv(x)
-
-        # pixel-wise qkv
-        # q_pix: (n, p^2, w^2, c_qk)
-        # kv_pix: (n, p^2, h_kv*w_kv, c_qk+c_v)
         q_pix = rearrange(q, 'n p2 h w c -> n p2 (h w) c')
         kv_pix = self.kv_down(rearrange(kv, 'n p2 h w c -> (n p2) c h w'))
         kv_pix = rearrange(kv_pix, '(n j i) c h w -> n (j i) (h w) c', j=self.n_win, i=self.n_win)
 
-        q_win, k_win = q.mean([2, 3]), kv[..., 0:self.qk_dim].mean(
-            [2, 3])  # window-wise qk, (n, p^2, c_qk), (n, p^2, c_qk)
+        q_win, k_win = q.mean([2, 3]), kv[..., 0:self.qk_dim].mean([2, 3])
 
-        ##################side_dwconv(lepe)##################
-        # NOTE: call contiguous to avoid gradient warning when using ddp
+        # 计算位置信息增强 lepe
         lepe = self.lepe(rearrange(kv[..., self.qk_dim:], 'n (j i) h w c -> n c (j h) (i w)', j=self.n_win,
                                    i=self.n_win).contiguous())
         lepe = rearrange(lepe, 'n c (j h) (i w) -> n (j h) (i w) c', j=self.n_win, i=self.n_win)
 
-        ############ gather q dependent k/v #################
-
-        r_weight, r_idx = self.router(q_win, k_win)  # both are (n, p^2, topk) tensors
-
-        kv_pix_sel = self.kv_gather(r_idx=r_idx, r_weight=r_weight, kv=kv_pix)  # (n, p^2, topk, h_kv*w_kv, c_qk+c_v)
+        # 路由选择
+        r_weight, r_idx = self.router(q_win, k_win)
+        kv_pix_sel = self.kv_gather(r_idx=r_idx, r_weight=r_weight, kv=kv_pix)
         k_pix_sel, v_pix_sel = kv_pix_sel.split([self.qk_dim, self.dim], dim=-1)
-        # kv_pix_sel: (n, p^2, topk, h_kv*w_kv, c_qk)
-        # v_pix_sel: (n, p^2, topk, h_kv*w_kv, c_v)
 
-        ######### do attention as normal ####################
-        k_pix_sel = rearrange(k_pix_sel, 'n p2 k w2 (m c) -> (n p2) m c (k w2)',
-                              m=self.num_heads)  # flatten to BMLC, (n*p^2, m, topk*h_kv*w_kv, c_kq//m) transpose here?
-        v_pix_sel = rearrange(v_pix_sel, 'n p2 k w2 (m c) -> (n p2) m (k w2) c',
-                              m=self.num_heads)  # flatten to BMLC, (n*p^2, m, topk*h_kv*w_kv, c_v//m)
-        q_pix = rearrange(q_pix, 'n p2 w2 (m c) -> (n p2) m w2 c',
-                          m=self.num_heads)  # to BMLC tensor (n*p^2, m, w^2, c_qk//m)
+        # 执行多头注意力机制
+        k_pix_sel = rearrange(k_pix_sel, 'n p2 k w2 (m c) -> (n p2) m c (k w2)', m=self.num_heads)
+        v_pix_sel = rearrange(v_pix_sel, 'n p2 k w2 (m c) -> (n p2) m (k w2) c', m=self.num_heads)
+        q_pix = rearrange(q_pix, 'n p2 w2 (m c) -> (n p2) m w2 c', m=self.num_heads)
 
-        # param-free multihead attention
-        attn_weight = (
-                              q_pix * self.scale) @ k_pix_sel  # (n*p^2, m, w^2, c) @ (n*p^2, m, c, topk*h_kv*w_kv) -> (n*p^2, m, w^2, topk*h_kv*w_kv)
+        # 计算注意力权重并应用
+        attn_weight = (q_pix * self.scale) @ k_pix_sel
         attn_weight = self.attn_act(attn_weight)
-        out = attn_weight @ v_pix_sel  # (n*p^2, m, w^2, topk*h_kv*w_kv) @ (n*p^2, m, topk*h_kv*w_kv, c) -> (n*p^2, m, w^2, c)
+        out = attn_weight @ v_pix_sel
+
         out = rearrange(out, '(n j i) m (h w) c -> n (j h) (i w) (m c)', j=self.n_win, i=self.n_win,
                         h=H // self.n_win, w=W // self.n_win)
-
         out = out + lepe
-        # output linear
+
+        # 输出线性层
         out = self.wo(out)
 
-        # NOTE: use padding for semantic segmentation
-        # crop padded region
+        # 如果使用自动填充，去掉填充区域
         if self.auto_pad and (pad_r > 0 or pad_b > 0):
             out = out[:, :H_in, :W_in, :].contiguous()
 
@@ -293,7 +236,6 @@ class BiLevelRoutingAttention(nn.Module):
             return out, r_weight, r_idx, attn_weight
         else:
             return rearrange(out, "n h w c -> n c h w")
-
 
 class Attention(nn.Module):
     """
@@ -338,7 +280,6 @@ class Attention(nn.Module):
 
         x = rearrange(x, 'n (h w) c -> n c h w', h=H, w=W)
         return x
-
 
 class AttentionLePE(nn.Module):
     """
@@ -581,9 +522,10 @@ class BiLevelRoutingAttention_nchw(nn.Module):
         return output
 
 
-# 输入 N C HW,  输出 N C H W
+# 输入 B C H W,  输出 B C H W
 if __name__ == '__main__':
-    block = BiLevelRoutingAttention_nchw(64).cuda()
-    input = torch.rand(1, 64, 64, 64).cuda() # 输入 B C H W
-    output = block(input)
-    print(input.size(), output.size())
+    bra = BiLevelRoutingAttention_nchw(32).cuda()
+    input = torch.rand(1, 32, 128, 128).cuda() # 输入 B C H W
+    output = bra(input)
+    print(input.size())
+    print( output.size())
