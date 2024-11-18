fdbc7cdb7ac9cf60e9a67f65fdd8519425436f9e
AIFengheshu <dingqs.ctgu@vip.163.com>
2024-11-18 20:51:37 +0800

    Update (CVPR 2021) CoTAttention.py

--- (CVPR 2021) CoTAttention.py
+++ (CVPR 2021) CoTAttention.py
@@ -1,21 +1,14 @@
-import numpy as np
 import torch
-from torch import flatten, nn
-from torch.nn import init
-from torch.nn.modules.activation import ReLU
-from torch.nn.modules.batchnorm import BatchNorm2d
+from torch import nn
 from torch.nn import functional as F
 
-# 论文地址：https://arxiv.org/pdf/2107.12292
-# 论文：Contextual Transformer Networks for Visual Recognition
-# 微信公众号：AI缝合术
-"""
-2024年全网最全即插即用模块,全部免费!包含各种卷积变种、最新注意力机制、特征融合模块、上下采样模块，
-适用于人工智能(AI)、深度学习、计算机视觉(CV)领域，适用于图像分类、目标检测、实例分割、语义分割、
-单目标跟踪(SOT)、多目标跟踪(MOT)、红外与可见光图像融合跟踪(RGBT)、图像去噪、去雨、去雾、去模糊、超分等任务，
-模块库持续更新中......
-https://github.com/AIFengheshu/Plug-play-modules
-"""
+# 论文题目：Contextual Transformer Networks for Visual Recognition
+# 中文题目:  视觉识别的上下文变换网络
+# 论文链接：https://arxiv.org/pdf/2107.12292
+# 官方github：https://github.com/JDAI-CV/CoTNet
+# 所属机构：京东AI研究院
+# 关键词：视觉识别、上下文信息、Transformer、自注意力机制、计算机视觉
+# 微信公众号：AI缝合术   https://github.com/AIFengheshu/Plug-play-modules
 
 class CoTAttention(nn.Module):
 
@@ -33,7 +26,6 @@ class CoTAttention(nn.Module):
             nn.Conv2d(dim,dim,1,bias=False),
             nn.BatchNorm2d(dim)
         )
-
         factor=4
         self.attention_embed=nn.Sequential(
             nn.Conv2d(2*dim,2*dim//factor,1,bias=False),
@@ -41,7 +33,6 @@ class CoTAttention(nn.Module):
             nn.ReLU(),
             nn.Conv2d(2*dim//factor,kernel_size*kernel_size*dim,1)
         )
-
 
     def forward(self, x):
         bs,c,h,w=x.shape
@@ -54,15 +45,11 @@ class CoTAttention(nn.Module):
         att=att.mean(2,keepdim=False).view(bs,c,-1) #bs,c,h*w
         k2=F.softmax(att,dim=-1)*v
         k2=k2.view(bs,c,h,w)
-
-
         return k1+k2
 
-
 if __name__ == '__main__':
-    input=torch.randn(50,512,7,7)
-    cot = CoTAttention(dim=512,kernel_size=3)
+    input=torch.randn(1,32,256,256)
+    cot = CoTAttention(dim=32,kernel_size=3)
     output=cot(input)
+    print(input.shape)
     print(output.shape)
-
-    
